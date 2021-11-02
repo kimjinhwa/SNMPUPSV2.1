a1ca19478d2262c628fe13f8423535ae4af4da9c
mvcjhkim <mvcjhkim@gmail.com>
2021-11-02 15:38:48 +0900

    한전의 웹을 Disable하는 루틴으로 23포트는 웹을 Disable한다. 이것은 IPFINDER로 다시 살릴수 있다

--- src/config/conf_eth.h
+++ src/config/conf_eth.h
@@ -92,15 +92,15 @@ to use an MII interface. */
 
 #ifdef IFTECH_ADDRESS
 /*! The ipaddress being used. */
-#define ETHERNET_CONF_IPADDR0                        100 
-#define ETHERNET_CONF_IPADDR1                        201 
-#define ETHERNET_CONF_IPADDR2                        192 
-#define ETHERNET_CONF_IPADDR3                        42  
+#define ETHERNET_CONF_IPADDR0                        192 
+#define ETHERNET_CONF_IPADDR1                        168 
+#define ETHERNET_CONF_IPADDR2                        0  
+#define ETHERNET_CONF_IPADDR3                        57  
 
 /*! The gateway address being used. */
-#define ETHERNET_CONF_GATEWAY_ADDR0                  100 
-#define ETHERNET_CONF_GATEWAY_ADDR1                  201 
-#define ETHERNET_CONF_GATEWAY_ADDR2                  192 
+#define ETHERNET_CONF_GATEWAY_ADDR0                  192 
+#define ETHERNET_CONF_GATEWAY_ADDR1                  168 
+#define ETHERNET_CONF_GATEWAY_ADDR2                  0 
 #define ETHERNET_CONF_GATEWAY_ADDR3                  1 
 
 /*! The network mask being used. */
